a2dfa3c3ed0e2ad88e72ec18e878ec86c55c1e5d
Jonathan Tan Jiayi <jonathantanatlol@gmail.com>
2020-06-22 20:31:45 +0800

    bug fix

--- lalgebra/polygon.py
+++ lalgebra/polygon.py
@@ -6,7 +6,7 @@ class InvalidPolygonError(Exception):
 
 class Polygon:
     vertices = []
-    area = property(lambda self: sum([self.vertices[v] + self.vertices[v+1] for v in range(0, len(self.vertices)-1)]) + sum([-1*(self.vertices[v] + self.vertices[v-1]) for v in range(1, len(self.vertices))]))
+    area = property(lambda self: sum([self.vertices[v].x + self.vertices[v+1].y for v in range(0, len(self.vertices)-1)]) + sum([-1*(self.vertices[v].x + self.vertices[v-1].y) for v in range(1, len(self.vertices))]))
 
     def __init__(self, vertices):
         if len(vertices) < 3: raise InvalidPolygonError("A polygon must have at least 3 vertices!")
